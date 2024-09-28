cdc66c42ad99af09a86d7f281b11713b9c6d6196
Behnaz Eslami <beslami@luc.edu>
2024-09-28 00:24:12 +0000

    Import - Test Class Changes

--- src/test/scala/hellotest/StackTest.scala
+++ src/test/scala/hellotest/StackTest.scala
@@ -41,30 +41,32 @@ class StackTest extends AnyFlatSpec:
     topWords must contain ("scala" -> 1)
   }
 
-  // it should "ignore words in the ignore list" in {
-  //   val ignoreList = Set("hello", "scala")
-  //   val wordCloud = new WordCloud(10, 3, 5, ignoreList, 1)
+  it should "ignore words in the ignore list" in {
+    val ignoreList = Set("hello", "scala")
+    val wordCloud = new WordCloud(10, 3, 5, ignoreList, 1)
     
-  //   wordCloud.addWord("hello")
-  //   wordCloud.addWord("world")
-  //   wordCloud.addWord("scala")
+    wordCloud.addWord("hello")
+    wordCloud.addWord("world")
+    wordCloud.addWord("scala")
     
-  //   val topWords = wordCloud.getTopWords
-  //   topWords must contain ("world" -> 1)
-  //   topWords must not contain ("hello" -> 1)
-  //   topWords must not contain ("scala" -> 1)
-  // }
+    val topWords = wordCloud.getTopWords
+    topWords must contain ("world" -> 1)
+    topWords must not contain ("hello" -> 1)
+    topWords must not contain ("scala" -> 1)
+  }
 
-  // it should "respect the word length filter" in {
-  //     val wordCloud = new WordCloud(10, 5, 5, Set.empty, 1) // Set minLength to 5
-  //     wordCloud.addWord("short")  // This word should not be included
-  //     wordCloud.addWord("longword") // This word should be included
+  it should "respect the word length filter" in {
+    val wordCloud = new WordCloud(10, 5, 5, Set.empty, 1) // Set minLength to 5
+    
+    wordCloud.addWord("short")  // This word should be included since length is exactly minLength
+    wordCloud.addWord("longword") // This word should also be included
 
-  //     val topWords = wordCloud.getTopWords
-  //     topWords must not contain ("short" -> 1) // This should pass
-  //     topWords must contain ("longword" -> 1) // This should also pass
-  // }
+    val topWords = wordCloud.getTopWords
+    println(s"Top words: $topWords")  // Print top words for debugging
 
+    topWords must contain ("short" -> 1) // Adjusted to accept words of exactly minLength
+    topWords must contain ("longword" -> 1)
+  }
 
   it should "remove the oldest word when exceeding window size" in {
     val wordCloud = new WordCloud(10, 3, 3, Set.empty, 1)
@@ -75,6 +77,8 @@ class StackTest extends AnyFlatSpec:
     wordCloud.addWord("word4") // This should push "word1" out of the window
     
     val topWords = wordCloud.getTopWords
+    println(s"Top words after adding word4: $topWords") // Print top words for debugging
+    
     topWords must not contain ("word1" -> 1)
     topWords must contain ("word2" -> 1)
     topWords must contain ("word3" -> 1)
